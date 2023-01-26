be06cce16292301935083075aa1b0aa5deff4944
Nate Wiltzius <nwiltzius@wisc.edu>
2023-01-26 13:27:16 -0700

    Remade project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
