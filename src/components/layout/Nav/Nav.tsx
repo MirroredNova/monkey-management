53741382e18607356b8389a9200a4806d4800370
Nate Wiltzius <nwiltzius@wisc.edu>
2023-02-01 20:25:27 -0600

    Added nav components

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import styles from './Nav.module.css';
+import styles from './nav.module.css';
 
 const Nav = () => (
   <nav className={styles.navbar}>
-    <h1>Monkey Management</h1>
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/blogs">Blogs</a>
-      </li>
-      <li>
-        <a href="/about">About Me</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
-      <li>
-        <a href="/projects">Projects</a>
-      </li>
-    </ul>
+    <div>
+      <h1>Monkey Management</h1>
+    </div>
+    <div>
+      <ul className={styles.navlist}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li>
+          <a href="/about">About Me</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+      </ul>
+    </div>
+
   </nav>
 );
 
